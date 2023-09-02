5b1004d51bc388880cb55d6de4b284332b9651ff
Alexander Long <alexanderlong5@gmail.com>
2023-09-02 10:40:04 -0600

    Working on input class

--- libs/sammyInput.py
+++ libs/sammyInput.py
@@ -1,4 +1,4 @@
-
+import configparser
 
 class InputFile:
 
@@ -18,29 +18,78 @@ class InputFile:
         return f"{data:>{width}d}"
 
     class Card1:
-        def __init__(self, TITLE):
+        def __init__(self, config_file=None):
+            self.TITLE = ""
+            if config_file:
+                self._read_from_config(config_file)
+        
+        def set(self, TITLE):
             self.TITLE = TITLE
-
+        
+        def _read_from_config(self, config_file):
+            config = configparser.ConfigParser()
+            config.read(config_file)
+            self.TITLE = config.get('Card1', 'TITLE', fallback=self.TITLE)
+            
         def __str__(self):
             return InputFile.format_type_A(self.TITLE, InputFile.MAX_COLUMNS)
 
     class Card2:
-        def __init__(self, ELMNT, AW, EMIN, EMAX, NEPNTS, ITMAX, ICORR, NXTRA, IPTDOP, IPTWID, IXXCHN, NDIGIT, IDROPP, MATNUM):
-            self.ELMNT = ELMNT      # Element name
-            self.AW = AW            # Atomic weight
-            self.EMIN = EMIN        # Minimum energy
-            self.EMAX = EMAX        # Maximum energy
-            self.NEPNTS = NEPNTS    # Number of points to be used in generating artificial energy grid
-            self.ITMAX = ITMAX      # Number of iterations (default = 2)
-            self.ICORR = ICORR      # Correlation option (default = 50)
-            self.NXTRA = NXTRA      # Number of extra points to be added between each pair of data points for auxiliary energy grid
-            self.IPTDOP = IPTDOP    # Number of points to be added to auxiliary energy grid across small resonances
-            self.IPTWID = IPTWID    # Determines the number of points to be added to auxiliary grid in tails of small resonances
-            self.IXXCHN = IXXCHN    # Number of energy channels in ODF-type data file to be ignored
-            self.NDIGIT = NDIGIT    # Number of digits for compact format for covariance matrix
-            self.IDROPP = IDROPP    # The input resonanceparameter covariance matrix will be modified before being used in the fitting procedure.
-            self.MATNUM = MATNUM    # ENDF Material number
+        def __init__(self, config_file=None):
+            # Default values
+            self.ELMNT = ""     # Element name
+            self.AW = 0         # Atomic weight
+            self.EMIN = 0       # Minimum energy
+            self.EMAX = 0       # Maximum energy
+            self.NEPNTS = 0     # Number of points to be used in generating artificial energy grid
+            self.ITMAX = 0      # Number of iterations (default = 2)
+            self.ICORR = 0      # Correlation option (default = 50)
+            self.NXTRA = 0      # Number of extra points to be added between each pair of data points for auxiliary energy grid
+            self.IPTDOP = 0     # Number of points to be added to auxiliary energy grid across small resonances
+            self.IPTWID = 0     # Determines the number of points to be added to auxiliary grid in tails of small resonances
+            self.IXXCHN = 0     # Number of energy channels in ODF-type data file to be ignored
+            self.NDIGIT = 0     # Number of digits for compact format for covariance matrix
+            self.IDROPP = 0     # The input resonanceparameter covariance matrix will be modified before being used in the fitting procedure.
+            self.MATNUM = 0     # ENDF Material number
 
+            if config_file:
+                self._read_from_config(config_file)
+            
+        def set(self, ELMNT=None, AW=None, EMIN=None, EMAX=None, NEPNTS=None, ITMAX=None, ICORR=None, NXTRA=None, IPTDOP=None, IPTWID=None, IXXCHN=None, NDIGIT=None, IDROPP=None, MATNUM=None):
+            if ELMNT is not None: self.ELMNT = ELMNT
+            if AW is not None: self.AW = AW
+            if EMIN is not None: self.EMIN = EMIN
+            if EMAX is not None: self.EMAX = EMAX
+            if NEPNTS is not None: self.NEPNTS = NEPNTS
+            if ITMAX is not None: self.ITMAX = ITMAX
+            if ICORR is not None: self.ICORR = ICORR
+            if NXTRA is not None: self.NXTRA = NXTRA
+            if IPTDOP is not None: self.IPTDOP = IPTDOP
+            if IPTWID is not None: self.IPTWID = IPTWID
+            if IXXCHN is not None: self.IXXCHN = IXXCHN
+            if NDIGIT is not None: self.NDIGIT = NDIGIT
+            if IDROPP is not None: self.IDROPP = IDROPP
+            if MATNUM is not None: self.MATNUM = MATNUM
+
+        def _read_from_config(self, config_file):
+            config = configparser.ConfigParser()
+            config.read(config_file)
+            self.ELMNT = config.get('Card2', 'ELMNT', fallback=self.ELMNT)
+            self.AW = config.getfloat('Card2', 'AW', fallback=self.AW)
+            self.EMIN = config.getfloat('Card2', 'EMIN', fallback=self.EMIN)
+            self.EMAX = config.getfloat('Card2', 'EMAX', fallback=self.EMAX)    
+            self.AW = config.getfloat('Card2', 'AW', fallback=self.AW)
+            self.NEPNTS = config.getint('Card2', 'NEPNTS', fallback=self.NEPNTS)
+            self.ITMAX = config.getint('Card2', 'ITMAX', fallback=self.ITMAX)
+            self.ICORR = config.getint('Card2', 'ICORR', fallback=self.ICORR)
+            self.NXTRA = config.getint('Card2', 'NXTRA', fallback=self.NXTRA)
+            self.IPTDOP = config.getint('Card2', 'IPTDOP', fallback=self.IPTDOP)
+            self.IPTWID = config.getint('Card2', 'IPTWID', fallback=self.IPTWID)
+            self.IXXCHN = config.getint('Card2', 'IXXCHN', fallback=self.IXXCHN)
+            self.NDIGIT = config.getint('Card2', 'NDIGIT', fallback=self.NDIGIT)
+            self.IDROPP = config.getint('Card2', 'IDROPP', fallback=self.IDROPP)
+            self.MATNUM = config.getint('Card2', 'MATNUM', fallback=self.MATNUM)
+            
         def __str__(self):
             return (
                 InputFile.format_type_A(self.ELMNT, 10) +
