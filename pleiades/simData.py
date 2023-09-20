b9ab062227f67c7a90fea3a7ca0c9fef6cebdc2c
Alexander Long <alexanderlong5@gmail.com>
2023-09-19 21:38:10 -0600

    working on simData libs

--- pleiades/simData.py
+++ pleiades/simData.py
@@ -1,6 +1,63 @@
 import configparser
+import numpy as np
+from scipy.interpolate import interp1d
+
+def create_transmission(energy_grid, xs_data, thickness, thickness_unit, density, density_unit):
+    """Create the transmission data for the given material based on interpolation of the 
+    cross-section data and the energy grid for a given material thickness and density.
+    This uses the the attenuation formula: T = e^(-sigma * A) where sigma is the cross-section,
+    and A is the areal density, which can be taken as thickness * density.
+
+    Args:
+        xs_data (tuple array): list of tuples containing the energy and cross-section data
+        thickness (float): thickness of the material
+        thickness_unit (string): unit of the thickness for the given material
+        density (float): density of the material
+        density_unit (string): unit of the density for the given material
+    """
+    
+    # TODO: Convert thickness and density to consistent units if necessary. 
+    # This is just an example; you'd need to provide conversion factors.
+    if thickness_unit != "cm":
+        if thickness_unit == "mm":
+            thickness /= 10.0
+        else:
+            raise ValueError("Unsupported thickness unit")
+    if density_unit != "g/cm3":
+        raise ValueError("Unsupported density unit")
+
+    # Create interpolation function for cross-section data
+    energies, cross_sections = zip(*xs_data)
+    interpolate_xs_data = interp1d(energies, cross_sections, kind='linear', fill_value="extrapolate")
+
+    # create a list of tuples containing the energy and transmission data
+    transmission = []
+    
+    # interoplate the cross-section data to get cross-sections and calculation transmission 
+    # at each point within the energy grid
+    for energy in energy_grid:
+        cross_section_at_energy = interpolate_xs_data(energy)
+        areal_density = thickness * density
+        T = np.exp(-cross_section_at_energy * areal_density)
+        transmission.append((energy, T))
+    
+    return transmission
+
 
 def parse_xs_file(file_location, isotope_name):
+    """ Parse the cross-section file and return the data for the isotope.
+
+    Args:
+        file_location (string): File location of the cross-section data
+        isotope_name (string): Name of the isotope to find in the file
+
+    Raises:
+        ValueError: If the isotope is not found in the file
+
+    Returns:
+        xs_data: List of tuples containing the energy and cross-section data
+    """
+    
     xs_data = []
     isotope_xs_found = False
     capture_data = False
@@ -32,6 +89,8 @@ def parse_xs_file(file_location, isotope_name):
 
 
 class Isotope:
+    """Class to hold information about an isotope from a config file.
+    """
     def __init__(self, name="Unknown", thickness=0.0, thickness_unit="atoms/cm2", abundance=0.0, xs_file_location="Unknown", density=0.0, density_unit="g/cm3"):
         self.name = name
         self.thickness = thickness
@@ -53,6 +112,8 @@ class Isotope:
         return f"Isotope({self.name}, {self.thickness} {self.thickness_unit}, {self.abundance}, {self.xs_file_location}, {self.density} {self.density_unit}, {xs_status})"
 
 class Isotopes:
+    """Class to hold information about all isotopes from a config file.
+    """
     def __init__(self, config_file):
         self.isotopes = []
         config = configparser.ConfigParser()
