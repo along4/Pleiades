7af0640f4999524eedf551716753073c5f5b90ea
Alexander Long <alexanderlong5@gmail.com>
2023-10-10 16:07:51 -0600

    changes to conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "PLEIADES"
+copyright = "2023, Alexander M. Long"
+author = "Alexander M. Long"
 
 
 # -- General configuration ---------------------------------------------------
