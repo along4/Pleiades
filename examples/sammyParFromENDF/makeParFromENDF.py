4b13790e7bf2ec5c1b3508cb2d0f68dbeeeb05e0
Alexander Long <alexlong@lanl.gov>
2024-03-12 16:43:52 -0600

    updating sammyParFromENDF example

--- examples/sammyParFromENDF/makeParFromENDF.py
+++ examples/sammyParFromENDF/makeParFromENDF.py
@@ -8,12 +8,14 @@ PWD = pathlib.Path(__file__).parent # get the location of this py file
 
 def main(config_inp_file: str="config_inp.ini"):
     
-    # Use the InputFile methods read config files and write inp files
+    # Use the InputFile methods read config file, process it into a input, and write an inp file
     sammy_input = psi.InputFile(config_inp_file)
-    sammy_input.process()
+    sammy_input.process()       
     sammy_input.write("ENDF.inp")    
 
+    # Use the run_endf funtion in sammyRunner class to create an ENDF.par file. 
     psr.run_endf(inpfile="ENDF.inp")
+    
     
 if __name__ == "__main__":
 
