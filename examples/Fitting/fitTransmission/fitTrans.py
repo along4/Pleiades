baac6b432e2943d927cc81800b3ea90951b0d228
Alexander Long <alexlong@lanl.gov>
2024-03-13 15:18:27 -0600

    working on fitTrans example

--- examples/Fitting/fitTransmission/fitTrans.py
+++ examples/Fitting/fitTransmission/fitTrans.py
@@ -1,5 +1,6 @@
 import pandas
 import argparse, sys
+import shutil
 import matplotlib.pyplot as plt
 
 import pleiades.sammyInput as psi 
@@ -10,12 +11,20 @@ import pleiades.sammyParFile as pspf
 
 def main(config_inp_file: str="config_inp.ini"):
     
+    # Creating file names for SAMMY inputs and parameters. 
+    run_name = config_inp_file.split(".")[0]
+    sammy_inp_file_name = run_name+".inp"
+    sammy_par_file_name = run_name+".par"
+    
     # Use the InputFile methods read config file, process it into a input, and write an inp file
     sammy_input = psi.InputFile(config_inp_file)
     sammy_input.process()      
+    sammy_input.write(sammy_inp_file_name)    
+
+    # Use the run_endf in sammyRunner to generate the needed par file.
+    psr.run_endf(inpfile=sammy_inp_file_name)
     
-    sammy_input_file_name = config_inp_file.split(".")[0]+".inp"
-    sammy_input.write(sammy_input_file_name)    
+    #psr.run(archivename=run_name,inpfile=sammy_inp_file_name,parfile=sammy_par_file_name,datafile="u235_1_to_100eV.twenty") 
 
 
 if __name__ == "__main__":
